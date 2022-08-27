5cc6600984e8f7ce5cbe35c219439e1192647d20
GaminnLinux <43827303+GamerHun1238@users.noreply.github.com>
2022-08-27 10:02:56 +0200

    test

--- src/main/kotlin/org/kamiblue/client/module/modules/movement/Flight.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/movement/Flight.kt
@@ -117,15 +117,62 @@ internal object Flight : Module(
             if(it.packet is CPacketPlayer) {
                 packets++;
                 if(packets >= 20) {
-                    val field = it.packet::class.java.getDeclaredField("onGround")
-                    field.isAccessible = true;
-                    field.set(it.packet::class, true);
-                    packets = 0;
+                    this.handleVanillaKickBypass();
                 }
             }
             if(it.packet is CPacketPlayerAbilities) {
                 it.packet.setFlying(false);
             }
         }
+    }
+    
+    fun handleVanillaKickBypass() {
+        val ground = calculateGround()
+        run {
+            var posY = mc.thePlayer.posY
+            while (posY > ground) {
+                mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX, posY, mc.thePlayer.posZ, true))
+                if (posY - 8.0 < ground) break // Prevent next step
+                posY -= 8.0
+            }
+        }
+        mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX, ground, mc.thePlayer.posZ, true))
+        var posY = ground
+        while (posY < mc.thePlayer.posY) {
+            mc.netHandler.addToSendQueue(C04PacketPlayerPosition(mc.thePlayer.posX, posY, mc.thePlayer.posZ, true))
+            if (posY + 8.0 > mc.thePlayer.posY) break // Prevent next step
+            posY += 8.0
+        }
+        mc.netHandler.addToSendQueue(
+            C04PacketPlayerPosition(
+                mc.thePlayer.posX,
+                mc.thePlayer.posY,
+                mc.thePlayer.posZ,
+                true
+            )
+        )
+    }
+    
+    private fun calculateGround(): Double {
+        val playerBoundingBox = mc.thePlayer.entityBoundingBox
+        var blockHeight = 1.0
+        var ground = mc.thePlayer.posY
+        while (ground > 0.0) {
+            val customBox = AxisAlignedBB(
+                playerBoundingBox.maxX,
+                ground + blockHeight,
+                playerBoundingBox.maxZ,
+                playerBoundingBox.minX,
+                ground,
+                playerBoundingBox.minZ
+            )
+            if (mc.theWorld.checkBlockCollision(customBox)) {
+                if (blockHeight <= 0.05) return ground + blockHeight
+                ground += blockHeight
+                blockHeight = 0.05
+            }
+            ground -= blockHeight
+        }
+        return 0.0
     }
 }
